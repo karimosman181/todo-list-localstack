7b5dffd87163c99596513e1aa3b729c6327819fe
Karim Osman <kareemosman199@gmail.com>
2025-07-20 11:06:24 +0000

    add access keys

--- backend/scripts/create-tables.js
+++ backend/scripts/create-tables.js
@@ -4,6 +4,8 @@ const AWS = require("aws-sdk");
 AWS.config.update({
   region: "us-east-1",
   endpoint: "http://localhost:4566",
+  accessKeyId: 'test',
+  secretAccessKey: 'test'
 });
 
 const dynamodb = new AWS.DynamoDB();
